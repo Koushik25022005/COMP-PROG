9bb954a587ec113eb6eb9cfcdfd0fdf48ed960b2
Koushik25022005 <sripathikoushik244@gmail.com>
2024-08-25 09:14:02 +0400

    Create COMP.cpp

---
+++ COMP.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+
+#define fi first
+#define se second
+//#define up unordered_map
+#define us unordered_set
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef long long ll;
+typedef pair<int,int> ii;
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<ll,null_type,less_equal<>,rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+//s.find_by_order(ele)--for it of target
+//s.order_of_key(ele)--for lower bound of target
+ll m1=1000000007;
+ll m2=998244353;
+
+
+
+
+void solve(){
+
+
+
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while(t-->0)  solve();
+}
